d377d725b22126d7b153810b87ba71d3df56b733
Jamie Connolly <jamie.connolly@tailify.com>
2018-12-22 21:49:17 +0000

    Use test method instead of it alias when testing

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,7 +3,7 @@
 const browserslist = require('browserslist');
 const config = require('../lib/index.js');
 
-it('should not contain invalid queries', () => {
+test('should not contain invalid queries', () => {
   const result = browserslist(config);
   expect(result).toBeTruthy();
 });
